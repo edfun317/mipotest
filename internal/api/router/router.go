7226127cd0237ba77517d2e6b1e3430bca8ed121
Ed <Ed@MacBook-Pro.local>
2025-06-28 22:35:20 +0800

    add routes

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"github.com/edfun317/mipotest/internal/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, h *handler.AccountHandler) {
+
+	r.POST("/accounts", h.CreateAccount)
+	r.POST("/accounts/:id/deposit", h.Deposit)
+	r.POST("/accounts/:id/withdraw", h.Withdraw)
+	r.POST("/accounts/transfer", h.Transfer)
+	r.GET("/accounts/:id", h.GetAccount)
+	r.GET("/accounts/:id/logs", h.GetTransactionLogs)
+}
