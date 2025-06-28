c98b8c92ce3573a67ef1299f56f6c9163474bb3b
Ed <Ed@MacBook-Pro.local>
2025-06-28 21:47:59 +0800

    add index

--- internal/domain/entity/account.go
+++ internal/domain/entity/account.go
@@ -11,10 +11,10 @@ type Account struct {
 }
 
 type TransactionLog struct {
-	ID            uint `gorm:"primaryKey"`
-	FromAccountID *uint
-	ToAccountID   *uint
-	Amount        int64 // positive only
-	CreatedAt     time.Time
+	ID            uint  `gorm:"primaryKey"`
+	FromAccountID *uint `gorm:"index:idx_from_account_time"`
+	ToAccountID   *uint `gorm:"index:idx_to_account_time"`
+	Amount        int64
+	CreatedAt     time.Time `gorm:"index:idx_from_account_time;index:idx_to_account_time"`
 	Description   string
 }
